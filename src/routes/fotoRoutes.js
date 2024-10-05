19ddd5b780b6f642331d8bedd41443970101b830
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-05 16:00:48 -0400

    chore: created routes send file

---
+++ src/routes/fotoRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import multer from "multer";
+import fotoController from "../controllers/FotoController";
+import multerConfig from "../config/multerConfig";
+
+const router = new Router();
+const upload = multer(multerConfig);
+
+router.post("/", upload.single("foto"), fotoController.store);
+
+export default router;
