43fa2dba9af5de17c4ff9f4bff910dd5708eec7d
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-04 23:54:08 -0400

    change: changes to the update and delete routes

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
-
-export default (req, res, next) => {
+import User from "../models/User";
+export default async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -14,6 +14,18 @@ export default (req, res, next) => {
   try {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
     const { id, email } = dados;
+
+    const user = await User.findAll({
+      where: {
+        id,
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(401).json({
+        errors: ["Usuário invalido"],
+      });
+    }
     req.userId = id;
     req.userEmail = email;
     return next();
