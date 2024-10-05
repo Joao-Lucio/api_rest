079827b3e924ec4603d4152267edccd04ac93e3a
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-04 22:42:05 -0400

    chore: created CRUD user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -11,6 +11,68 @@ class UserController {
         .json({ errors: e.errors.map((err) => err.message) });
     }
   }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ errors: ["ID não enviado."] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ errors: ["Usurário não encontrado."] });
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ errors: ["ID não enviado."] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ errors: ["Usurário não encontrado."] });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new UserController();
