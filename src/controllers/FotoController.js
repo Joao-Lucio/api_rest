6dfc8824b2d8ea879a4d13a39f8e64a062d1af37
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-05 16:49:25 -0400

    chore: added try catch

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -11,10 +11,17 @@ class FotoController {
           errors: [err.code],
         });
       }
-      const { originalname, filename } = req.file;
-      const { aluno_id } = req.body;
-      const foto = await Foto.create({ originalname, filename, aluno_id });
-      return res.json(foto);
+
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await Foto.create({ originalname, filename, aluno_id });
+        return res.json(foto);
+      } catch (e) {
+        res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
     });
   }
 }
