bab616d7c7f5e79496d4267486f9ae219b37a292
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-05 17:27:31 -0400

    chore: added dist config

---
+++ dist/database/seeds/20241005035514-criar-usuarios.js
@@ -0,0 +1,38 @@
+"use strict";"use strict";
+
+const bcryptjs = require("bcryptjs");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    return await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          nome: "Luiz",
+          email: "luiz1@gmail.com",
+          password_hash: await bcryptjs.hash("123456", 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: "Frango",
+          email: "frango@gmail.com",
+          password_hash: await bcryptjs.hash("123456", 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: "Messi",
+          email: "messi1@gmail.com",
+          password_hash: await bcryptjs.hash("123456", 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down() {},
+};
