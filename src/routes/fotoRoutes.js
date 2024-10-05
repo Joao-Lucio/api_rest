0058d89b758d3b212cc8d64d576c39f9ffe8a130
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-05 16:10:47 -0400

    chore: added validate type file

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,11 +1,8 @@
 import { Router } from "express";
-import multer from "multer";
 import fotoController from "../controllers/FotoController";
-import multerConfig from "../config/multerConfig";
 
 const router = new Router();
-const upload = multer(multerConfig);
 
-router.post("/", upload.single("foto"), fotoController.store);
+router.post("/", fotoController.store);
 
 export default router;
